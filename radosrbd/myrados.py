ed21dbff7465f57146db48feca87c39082162cc0
ljw <1797702937@qq.com>
2019-09-04 16:29:11 +0800

    m

--- radosrbd/myrados.py
+++ radosrbd/myrados.py
@@ -3,15 +3,15 @@ import rbd
 class myrados:
     def __init__(self,ceph_conf_path):
         self.cluster=rados.Rados(conffile=ceph_conf_path)
-    def connect():
+    def connect(self):
         try:
             self.cluster.connect()
         except Exception as e:
             print("connect error:",e)
         print("connect to the cluster")
-    def listpool():
+    def listpool(self):
         pools=self.cluster.list_pools()
-        print("pools {pool}".format(pool=pool1))
+        print("pools {pool}".format(pool=pools))
 if __name__ == "__main__":
     mycluster=myrados("/etc/ceph/ceph.conf")
     mycluster.connect()
